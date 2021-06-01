10f37339c5df9a8f06efc89f1c8948242e1db2f2
Felipe Machado <felipemmachado28@gmail.com>
2021-06-01 10:15:19 -0300

    alteração na validação do formulario

--- src/services/Formulario/CriarFormulario/CriarFormularioService.spec.ts
+++ src/services/Formulario/CriarFormulario/CriarFormularioService.spec.ts
@@ -5,7 +5,6 @@ import OpcoesResposta from '@schemas/OpcaoResposta';
 import Formulario from '@schemas/Formulario';
 import MongoMock from '@shared/tests/MongoMock';
 import CriarFormularioService from '@services/Formulario/CriarFormulario/CriarFormularioService';
-import ValidatorError from '@shared/errors/ValidatorError';
 
 describe('Formulario', () => {
   beforeAll(async () => {
@@ -22,7 +21,6 @@ describe('Formulario', () => {
     await OpcoesResposta.deleteMany({});
   });
 
-  /*
   it('deve retornar erro ao tentar criar um formulario sem nome', async () => {
     const criarFormularioService = container.resolve(CriarFormularioService);
 
@@ -30,26 +28,13 @@ describe('Formulario', () => {
     const descricao = 'Formulário que faz várias coisas';
     const publicado = false;
 
-    expect(await criarFormularioService.executar({ nome, descricao, publicado }))
-      .toThrow(expect.objectContaining({ nome: ['The nome field is required.'] }));
-
-    /*
-    expect(async () => {
-      await criarFormularioService.executar({ nome, descricao, publicado });
-    })
-      .toThrow((error: Error) => {
-        const result = error instanceof ValidatorError;
-        return result;
-      }); */
-
-  /*
     try {
       await criarFormularioService.executar({ nome, descricao, publicado });
       expect(true).toBe(false);
     } catch (e) {
       expect(true).toBe(true);
     }
-  }); */
+  });
 
   it('deve ser capaz de salvar um novo formulario', async () => {
     const criarFormularioService = container.resolve(CriarFormularioService);
